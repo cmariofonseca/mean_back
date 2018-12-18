faa7d8375ad6be9ae4c9b29261529a022bcdd706
cmariofonseca <cmariofonseca@gmail.com>
2018-12-17 22:16:20 -0500

    1 #done #time 1h #comment Se realizaron todos los comentarios necesarios para una mejor interpretación del código

--- routes/employee.route.js
+++ routes/employee.route.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const employee = require('../controllers/employee.controller');
 
-router.post('/', employee.createEmployee);
-router.get('/:id', employee.getEmployee);
+// se define el formato de ruta por el cual se enviará y obtendrá datos.
 router.get('/', employee.getEmployees);
+router.post('/', employee.createEmployee);
+// Cuando despues del slash se indica :id, esto informa que el método requiere id.
+router.get('/:id', employee.getEmployee);
 router.put('/:id', employee.editEmployee);
 router.delete('/:id', employee.deleteEmployee);
 
+// Archivo exportable.
 module.exports = router;
